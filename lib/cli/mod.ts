30538025633d903a167103e19b3810ed7bf8a320
Zuri Klaschka <pklaschka@users.noreply.github.com>
2024-09-06 22:54:26 +0200

    Export APIs

---
+++ lib/cli/mod.ts
@@ -0,0 +1,3 @@
+export * from "$cli/generate.ts";
+export * from "$cli/server.ts";
+export * from "$cli/help.ts";
--- main.ts
+++ main.ts
@@ -5,6 +5,10 @@ import { runServer } from "$cli/server.ts";
 import { printHelp } from "$cli/help.ts";
 import { runGenerate } from "$cli/generate.ts";
 
+export * from "$common/mod.ts";
+export * from "$http/mod.tsx";
+export * from "$cli/mod.ts";
+
 if (import.meta.main) {
   await validateDB();
   const args = parseArgs(Deno.args);
