fdc02c9cee0e891e1a78669ca5077a53d415b614
Zuri Klaschka <pklaschka@users.noreply.github.com>
2024-09-07 01:08:53 +0200

    Fix host name issues

--- lib/cli/server.ts
+++ lib/cli/server.ts
@@ -5,14 +5,14 @@ import metadata from "$/deno.json" with { type: "json" };
 
 const serverArgs = z.object({
   port: z.number().default(CONFIG.PORT),
-  host: z.string().default("localhost"),
+  host: z.string().default('0.0.0.0'),
 });
 
 /**
  * Runs the web server.
  * @param args the arguments to the command
  * @param args.port the port to listen on (default: the PORT environment variable)
- * @param args.host the hostname to listen on (default: localhost)
+ * @param args.host the hostname to listen on (default: 0.0.0.0)
  */
 export function runServer(args: unknown) {
   console.log(`Running ${metadata.name} v${metadata.version}`);
