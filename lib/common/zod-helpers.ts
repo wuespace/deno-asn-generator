e1ad258578e81249b6b1e2d4c6f35b18d7a35b33
Zuri Klaschka <pklaschka@users.noreply.github.com>
2024-09-09 14:34:29 +0200

    Add `"enabled"` as truthy value for configuration

--- lib/common/zod-helpers.ts
+++ lib/common/zod-helpers.ts
@@ -4,7 +4,7 @@ export function zBoolString(): z.ZodEffects<ZodBoolean, boolean, unknown> {
   return z.preprocess((val) => {
     if (
       typeof val === "string" &&
-      ["1", "true", "yes", "on"].includes(val.toLowerCase())
+      ["1", "true", "yes", "on", "enabled"].includes(val.toLowerCase())
     ) return true;
     return false;
   }, z.boolean());
