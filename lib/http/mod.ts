96ad6cfadda2cc87fce5fda8db89b3a984981f64
Zuri Klaschka <pklaschka@users.noreply.github.com>
2024-12-01 12:58:22 +0100

    style: :rotating_light: Remove unnecessary import

--- lib/http/mod.ts
+++ lib/http/mod.ts
@@ -15,7 +15,6 @@ import { svgRoutes } from "$http/routes/svg.ts";
 import { uiRoutes } from "$http/routes/ui.tsx";
 import { apiRoutes } from "$http/routes/api.ts";
 import { lookupRoutes } from "$http/routes/lookup.ts";
-import { withUser } from "$http/middleware/oidc.ts";
 
 export * from "$http/lookup-url.ts";
 export * from "$http/barcode-svg.ts";
